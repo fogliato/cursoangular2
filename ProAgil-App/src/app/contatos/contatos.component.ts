73dfc43397d5be244c3c40a1c699c7d30a9bd021
Luiz Fogliato <luiz.fogliato@meta.com.br>
2020-08-09 22:45:08 -0300

    Ajuste para colcoar editor de texto

--- ProAgil-App/src/app/contatos/contatos.component.ts
+++ ProAgil-App/src/app/contatos/contatos.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contatos',
   templateUrl: './contatos.component.html',
-  styleUrls: ['./contatos.component.css']
+  styleUrls: ['./contatos.component.css'],
 })
 export class ContatosComponent implements OnInit {
+  registerForm: FormGroup;
   title = 'Contatos';
-  constructor() { }
+
+  constructor(
+    private fb: FormBuilder,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.validation();
   }
 
+  executarContato() {}
+  validation() {
+    this.registerForm = this.fb.group({
+      nomeCompleto: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
 }
